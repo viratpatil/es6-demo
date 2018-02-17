7bedf37909e68e16127bdde48162e818c11f0e61
viratpatil <viratpatil@gmail.com>
2018-02-17 15:34:59 +0530

    added var and let

--- src/features/scoping.js
+++ src/features/scoping.js
@@ -5,6 +5,19 @@ export const usingLet = () => {
         console.log(a);
     }
     innerF();
-    console.log(b); // this will give error
+    console.log(b); // this will give an error
 
+    if (true)
+    {
+      let c = 4;   
+    }
+
+    console.log(c); // this will give an error
+
+    if (true)
+    {
+        var d = 5;
+    }
+    
+    console.log(d); // this will run correct as it is in the scope
 }
